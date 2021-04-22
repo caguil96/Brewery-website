190293a41d1f676a4158eaefed8f8dc6da808e76
Leonardo Aguilar <Leonardoaguilar@leonardos-air.lan>
2021-04-21 22:54:04 -0400

    still working on css part 2

--- script.js
+++ script.js
@@ -42,44 +42,8 @@ function menuOptionsState(breweries) {
 }
 
 
-// Create a function called listBreweries to get the complete list of data from API
-// Create a forEach loop inside the function to iterate through each "brewery" fom the list of "breweries"
 
 
-// Grab the ID from by_city and assign it to a new variable called cityButton
-// Create an event which is triggered by a click
-// Send a message once cityButton is clicked
-
-
-
-
-// Grab the ID from by_distance and assign it to a new variable called distanceButton
-// Create an event which is triggered by a click
-// Send a message once distanceButton is clicked
-
-
-
-
-// Grab the ID from by_name and assign it to a new variable called nameButton
-// Create an event which is triggered by a click
-// Send a message once cityButton is clicked
-
-
-
-
-// Grab the ID from by_state and assign it to a new variable called stateButton
-// Create an event which is triggered by a click
-// Send a message once stateButton is clicked
-
-
-// Grab the ID from by_postal_code and assign it to a new variable called postalButton
-// Create an event which is triggered by a click
-// Send a message once postalButton is clicked
-
-
-// Grab the ID from by_type and assign it to a new variable called typeButton
-// Create an event which is triggered by a click
-// Send a message once typeButton is clicked
 
 
 
