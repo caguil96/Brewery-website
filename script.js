17bb6d96a1f47329603f00e479820f00c455943a
Leonardo Aguilar <Leonardoaguilar@leonardos-air.lan>
2021-04-22 16:47:51 -0400

    working on helper function

--- script.js
+++ script.js
@@ -15,47 +15,30 @@ async function makeRequestState() {
 }
 
 makeRequestState();
-function menuOptionsState(breweries) {
-  const states = breweries.map((brewery) => {
 
+function menuOptionsState(breweries) {   // list of breweries passed in as an argument 
+  const states = breweries.map((brewery) => { // creates new array called states
     return brewery.state;
-
   })
-  let uniqueStates = states.filter((val, index, val2) => {
+  let uniqueStates = states.filter((val, index, val2) => { // why do I need to make a copy if I already have the array called states
     return val2.indexOf(val) == index;
-
   })
-  console.log(uniqueStates);
-  let stateDropdown = document.querySelector('#search-state');
-  uniqueStates.forEach((state) => {
-    let stateOption = document.createElement('option');
-    if (state === null) {
+  console.log(uniqueStates); // print variable called uniqueStates
+  let stateDropdown = document.querySelector('#search-state'); // select id and assign it to the new variable called stateDropDown
+  uniqueStates.forEach((state) => {  // select the array called uniqueStates and loop through each state
+    let stateOption = document.createElement('option'); // create an element tag called option and assign it to the new variable called stateOption
+    if (state === null) { // if the state variable has a value of null then return false
       return false;
     } else {
-      stateOption.innerText = state;
-      stateOption.value = state;
-      stateDropdown.append(stateOption);
+      stateOption.innerText = state; // include the name of the state to the space between the tags
+      stateOption.value = state; // what is this doing? 
+      stateDropdown.append(stateOption); // grab the option element and append it to the stateDropDown variable
     }
-
   })
-
 }
 
-
-
-
-
-
-
-
-
-
-// Callback function
-/*
-function getPosts(){
-  setTimeout(() => {
-
-
-  }, 1000);
-}
-*/
+// Create an event listener 
+// Create a function that stores the data selected 
+const button = document.getElementById('search');
+console.log(button);
+button.addEventListener('click', makeRequestState);
